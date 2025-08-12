1bbbce9f15741294e96d2b9f284ac90e59b1680e
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-08-13 01:05:53 +0300

    Switch to HashRouter for compatibility with GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.scss';
-import {Routes, Route,  BrowserRouter} from 'react-router-dom';
+import {Routes, Route, HashRouter} from 'react-router-dom';
 import CocktailsContainer from './containers/CocktailsContainer/CocktailsContainer';
 import {NotFoundComponent} from './components/NotFoundComponent/NotFoundComponent';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<CocktailsContainer />} />
                 <Route path="/not-found" element={<NotFoundComponent/>} />
                 <Route path="/:cocktailType" element={<CocktailsContainer />} />
                 <Route path="*" element={<NotFoundComponent/>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
